35b25bb0a7686ed7cdeef8aa49f229be6ea3b98e
jcastle <joseph.castle@gsa.gov>
2020-05-19 11:04:45 -0400

    Updates to Object file

--- chapterX_objects/playground.js
+++ chapterX_objects/playground.js
@@ -1,39 +1,42 @@
-// Add to chapter on objects
+console.log("here");
 
-// 1. Object constructor
-let obj = new Object();
 
-// 2. Object's create method
-let obj = new Object.create(obj);
+// // Add to chapter on objects
 
-// 3. Object literal syntax
-let obj = {};
+// // 1. Object constructor
+// let obj = new Object();
 
-// 4. Function constructor
-function employee(name){
-    let obj = {};
-    name;
-    obj.age = 18;
-    obj.salary = 40000;
-    return obj;
-}
-let obj = new employee('Don');
+// // 2. Object's create method
+// // let obj = new Object.create(obj);
 
-// 5. Function constructor with prototype
-function Employee() {
-    Employee.prototype.name = "Dom";
-    let object = new Employee();
-}
+// // 3. Object literal syntax
+// let obj = {};
 
-// 6. ES6 Class syntax
-class Employee {
-    constructor(name) {
-        this.name = name;
-    }
-}
-let obj = new Employee("Don");
+// // 4. Function constructor
+// function employee(name){
+//     let obj = {};
+//     name;
+//     obj.age = 18;
+//     obj.salary = 40000;
+//     return obj;
+// }
+// let obj = new employee('Don');
 
-// 7. Singleton pattern
-let object = new (function () {
-    this.name = "Don";
-    })();
+// // 5. Function constructor with prototype
+// function Employee() {
+//     Employee.prototype.name = "Dom";
+//     let object = new Employee();
+// }
+
+// // 6. ES6 Class syntax
+// class Employee {
+//     constructor(name) {
+//         this.name = name;
+//     }
+// }
+// let obj = new Employee("Don");
+
+// // 7. Singleton pattern
+// let object = new (function () {
+//     this.name = "Don";
+//     })();

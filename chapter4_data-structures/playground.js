604b60a672a596093bc5a3047d41e476dd3d92b4
jcastle <joseph.castle@gsa.gov>
2020-05-26 13:00:44 -0400

    Updates to Ch4

--- chapter4_data-structures/playground.js
+++ chapter4_data-structures/playground.js
@@ -3,3 +3,11 @@ console.log('chapter 4 - data structures - objects & arrays');
 let listOfNumbers = [2, 3, 5, 7, 11];
 console.log(listOfNumbers[2]);
 console.log(listOfNumbers[0]);
+
+// push and pop
+let sequence = [1, 2, 3];
+sequence.push(4);
+sequence.push(5);
+console.log(sequence);
+console.log(sequence.pop());
+console.log(sequence);
